da08a407a7e20b5a3dab4aaec1e57bab4b130a00
spanri <kozlova9v@mail.ru>
2020-01-19 17:55:58 +0300

    front: fix gitignore

---
+++ dist/_nuxt/01bb8a52d630618a9a4a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{306:function(t,e,n){"use strict";n.r(e);var l=n(10),component=Object(l.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("div",{staticClass:"profile"},[this._v("PROFILE")])}),[],!1,null,null,null);e.default=component.exports}}]);
